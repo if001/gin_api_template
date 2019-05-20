d2f5717e284399e38d05b99a2d149f2ad8fed161
Unknown <otomijuf.004@gmail.com>
2019-05-20 16:21:16 +0900

    取得したuserをcontextに保持する

--- interfaces/api/middleware/auth_required.go
+++ interfaces/api/middleware/auth_required.go
@@ -3,16 +3,29 @@ package middleware
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"fmt"
 )
 
+type User struct {
+	Name     string
+	Password string
+}
+
+func getUser(token string) User { // move to application layer
+	user := User{
+		Name:"hoge",
+		Password:"huga",
+	}
+	return user
+}
 func AuthRequired(c *gin.Context) {
 	token := c.Query("token")
-	panic("auth panic")
 	if token == "hogehoge" {
-		c.JSON(http.StatusOK, http.StatusText(http.StatusOK))
-		c.Next()
+		fmt.Println("authorize ok!")
+		user := getUser(token)
+		c.Set(gin.AuthUserKey, user) // set login user
+		return
 	}
 	c.JSON(http.StatusUnauthorized, http.StatusText(http.StatusUnauthorized))
 	c.Abort()
 }
-
